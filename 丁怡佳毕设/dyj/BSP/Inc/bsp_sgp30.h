7284b6b337e6dcb9c7c46d3dd8536462df23d775
zero <401660586@qq.com>
2020-04-02 23:55:56 +0800

    增加sgp30板级层代码

---
+++ 丁怡佳毕设/dyj/BSP/Inc/bsp_sgp30.h
@@ -0,0 +1,38 @@
+#ifndef _BSP_SGP30_H
+#define _BSP_SGP30_H
+
+#include "bsp.h"
+
+//sgp30I2C地址
+#define SGP30_ADDRESS 0x58 
+
+#define INIT_AIR_QUALITY        0X2003          /*初始化SGP30*/
+#define MEASURE_AIR_QUALITY     0X2008          /*SGP30开始测量*/
+#define GET_BASELINE            0X2015          /*获取SGP30基准线*/
+#define SET_BASELINE            0X201E          /*设置SGP30基准线*/
+#define SET_HUMIDITY            0X2061          /*设置绝对湿度*/
+//#define MEASURE_TEST            0X2032          /*运行片上自测试*/
+#define GET_FEATURE_SET_VERSION 0X202F          /*获取功能集版本*/
+#define MEASURE_RAW_SIGNALS     0X2050          /*测量的原始信号*/
+
+//IO口引脚宏定义
+#define SGP30_I2C_SCL       PAout(3) 
+#define SGP30_I2C_SDA_IN    PAin(4)
+#define SGP30_I2C_SDA_OUT   PAout(4)
+
+
+//IO方向设置
+#define SGP30_IO_IN()  {GPIOA->MODER&=~(3<<(4*2));GPIOA->MODER|=0<<4*2;}	//PA4输入模式
+#define SGP30_IO_OUT() {GPIOA->MODER&=~(3<<(4*2));GPIOA->MODER|=1<<4*2;} 	//PA4输出模式
+/*函数声明*/
+
+void bsp_sgp30_io_init(void);
+
+void bsp_sgp30_test(void);
+
+void bsp_sgp30_out(void);
+
+void bsp_sgp30_in(void);
+
+#endif
+
---
+++ 丁怡佳毕设/dyj/BSP/Src/bsp_sgp30.c
@@ -0,0 +1,144 @@
+#include "bsp_sgp30.h"
+#include "bsp.h"
+
+Swi2c_T t_sgp30_swi2c ={
+	(unsigned long *)&SGP30_I2C_SCL,
+	(unsigned long *)&SGP30_I2C_SDA_OUT,
+	(unsigned long *)&SGP30_I2C_SDA_IN,
+	bsp_sgp30_in,
+	bsp_sgp30_out
+};
+
+static void bsp_sgp30_out(void);
+
+static void bsp_sgp30_in(void);
+
+/*sgp30设置读写状态头部函数*/
+static void bsp_sgp30_header(uint8_t _ucWR);
+/*发送sgp30 2字节命令*/
+static void bsp_sgp30_comm(uint16_t _usComm);
+/*发送sgp30 数据*/
+static void bsp_sgp30_write_data(uint8_t* _ucTable, uint8_t length);
+/*读取sgp30 数据*/
+static void bsp_sgp30_read_data(uint8_t* _ucTable, uint8_t length);
+
+static void bsp_sgp30_sensor_init(void);
+
+void bsp_sgp30_io_init(void)
+{
+    GPIO_InitTypeDef  GPIO_InitStructure;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);//使能GPIOA时钟
+
+	//GPIOA3,4初始化设置
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4;//LED0和LED1对应IO口
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;//普通输出模式
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;//推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100MHz
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//上拉
+	GPIO_Init(GPIOA, &GPIO_InitStructure);//初始化GPIO
+
+    GPIO_SetBits(GPIOA, GPIO_Pin_3 | GPIO_Pin_4);//GPIOA3,4置高
+
+	bsp_sgp30_sensor_init();
+}
+
+
+static void bsp_sgp30_out(void)
+{
+    SGP30_IO_OUT()
+}
+
+static void bsp_sgp30_in(void)
+{
+    SGP30_IO_IN()
+}
+
+/*sgp30设置读写状态头部函数*/
+
+static void bsp_sgp30_header(uint8_t _ucWR)
+
+{
+	swi2cStart(&t_sgp30_swi2c);											/*I2C开始信号*/
+
+	swi2cSendByte(&t_sgp30_swi2c, (SGP30_ADDRESS << 1) | (_ucWR & (0x01)));/*发送sgp30头部信息*/
+ 
+	swi2cWaitAck(&t_sgp30_swi2c, 1);									/*等待sgp30的ack*/
+}
+
+/*发送sgp30 2字节命令*/
+
+static void bsp_sgp30_comm(uint16_t _usComm)
+
+{
+	swi2cSendByte(&t_sgp30_swi2c, _usComm >> 8);						/*发送sgp30命令高八位信息*/
+
+	swi2cWaitAck(&t_sgp30_swi2c, 1);									/*等待sgp30的ack*/
+ 
+ 	swi2cSendByte(&t_sgp30_swi2c, (uint8_t)_usComm);					/*发送sgp30命令低八位信息*/
+
+	swi2cWaitAck(&t_sgp30_swi2c, 1);									/*等待sgp30的ack*/
+}
+
+/*发送sgp30 数据*/
+
+static void bsp_sgp30_write_data(uint8_t* _ucTable, uint8_t length)
+
+{
+	for(int i = 0; i < length; i++)
+	{
+		swi2cSendByte(&t_sgp30_swi2c, _ucTable[i]);							/*发送sgp30命令高八位信息*/
+
+		swi2cWaitAck(&t_sgp30_swi2c, 1);									/*等待sgp30的ack*/
+	}
+
+}
+
+/*读取sgp30 数据*/
+
+static void bsp_sgp30_read_data(uint8_t* _ucTable, uint8_t length)
+
+{
+	for(int i = 0; i < length; i++)
+	{
+		_ucTable[i] = swi2cReceiveByte(&t_sgp30_swi2c);						/*读取sgp30的信息*/
+
+		swi2cAck(&t_sgp30_swi2c, i < (length - 1) ? 1 : 0);						/*master发送ack*/
+	}
+								
+}
+
+
+
+static void bsp_sgp30_sensor_init(void)
+{
+	bsp_sgp30_header(0);
+
+	bsp_sgp30_comm(INIT_AIR_QUALITY);
+
+	swi2cStop(&t_sgp30_swi2c);
+}
+
+uint8_t g_ucTable[6] = {0};
+
+void bsp_sgp30_test(void)
+{
+    bsp_sgp30_header(0);
+
+	bsp_sgp30_comm(MEASURE_AIR_QUALITY);
+	
+	swi2cStop(&t_sgp30_swi2c);
+	
+	delay_ms(100);
+	
+	bsp_sgp30_header(1);
+
+	bsp_sgp30_read_data(g_ucTable, 6);
+	
+	swi2cStop(&t_sgp30_swi2c);
+
+	
+}
+
+
+
